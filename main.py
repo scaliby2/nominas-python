a982f79b7725d636bd33992b1a06ca0b9e9b8a9f
jose de jesus <joses79.jdjm@gmail.com>
2025-12-26 12:21:49 -0600

    creacion del api

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+from fastapi import FastAPI
 from nomina.fijo import Fijo
 from nomina.freelancer import Freelancer
 
-nombre = input("Nombre del Trabajador:")
-apellido = input("Apellido del Trabajador:")
-trabajo = int(input("1 Para FreeLancer y 2 para Empleado fijo"))
-empleado = None
-if trabajo == 1:
-    sueldoFree = float(input("Ingresa tu Tarifa por hora: "))
-    horasFree = float(input("Ingresa las horas laboraras "))
-    empleado =Freelancer(nombre,apellido,horasFree,sueldoFree)
-    empleado.calcular_pago()
-elif trabajo == 2:
-    sueldoFijo = float(input("Ingresa tu sueldo fijo, antes de impuestos:"))
-    empleado = Fijo(nombre,apellido,sueldoFijo)
-    empleado.calcular_pago()
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return{"Sistema": "Nomina v 1.0", "Estado" : "Funcionando"}
+
+@app.get("/empleadofijo")
+def Calcular_fijo(nombre: str, apellido: str, sueldo: float):
+    try:
+        empleado = Fijo(nombre,apellido,sueldo)
+        mensaje_pago = empleado.calcular_pago()     
+        return {
+            "Status": "ok",
+            "Tipo" : "Empleado Fijo",
+            "Nombre_Completo" : f"{nombre} {apellido}",
+            "Resultado" : mensaje_pago
+        }
+    except ValueError as e:
+        return {"status" : "error", "mensaje" : str(e)}
+
+@app.get("/empleadofree")
+def calcular_free(nombre : str, apellido:str, horas: float, tarifa: float ):
+    try:
+        empleado = Freelancer(nombre,apellido,horas,tarifa)
+        mensaje_pago = empleado.calcular_pago()
+        
+        return{
+            "Status": "ok",
+            "Tipo" : "Empleado FreeLancer",
+            "Nombre Completo" : f"{nombre} {apellido}",
+            "Resultado" : mensaje_pago
+        }
+    except ValueError as e:
+        return {"Status": "error", "mensaje": str(e)}
